fa33013cf85a6bd22f34cf82aae4e03f94b87cff
Ashwini Bari <ashwinibari@google.com>
2024-11-07 14:25:52 +0530

    profileModal

--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -1,22 +1,81 @@
-import { Box, Button, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
+import { ChatState } from "../../context/ChatProvider";
+import ProfileModal from "./ProfileModal";
 
 const SideDrawer = () => {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
+
+  const { user } = ChatState();
+
   return (
     <>
-      <Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        bg="white"
+        w="100%"
+        p="5px 10px"
+        borderWidth="5px"
+      >
         <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
           <Button variant="ghost">
-            <i class="fas fa-search"></i>
+            <i className="fas fa-search"></i>
             <Text display={{ base: "none", md: "flex" }} px="4">
               Search User
             </Text>
           </Button>
         </Tooltip>
+
+        <Text
+          fontSize="2xl"
+          fontFamily="Work sans"
+          position="absolute"
+          left="50%"
+          transform="translateX(-50%)"
+        >
+          Talk-A-Tive
+        </Text>
+
+        <Box display="flex" alignItems="center" ml="auto">
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon w={6} h={6} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                {/* <MenuItem>My Profile</MenuItem> */}
+              </ProfileModal>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
       </Box>
     </>
   );
